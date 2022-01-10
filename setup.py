9e98d51e6ec29468ed68c96638d6bd11febf4ab8
jarbasai <jarbasai@mailfence.com>
2022-01-10 19:55:37 +0000

    :tada:

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+# skill_id=package_name:SkillClass
+PLUGIN_ENTRY_POINT = 'skill-fff.jarbasai=skill_fff:FullFreeFilmsSkill'
+
+setup(
+    # this is the package name that goes on pip
+    name='ovos-skill-fff',
+    version='0.0.1',
+    description='ovos Full Free Films skill plugin',
+    url='https://github.com/JarbasSkills/skill-fff',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    package_dir={"skill_fff": ""},
+    package_data={'skill_fff': ['locale/*', 'ui/*']},
+    packages=['skill_fff'],
+    include_package_data=True,
+    install_requires=["ovos_workshop~=0.0.5a1"],
+    keywords='ovos skill plugin',
+    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
+)
