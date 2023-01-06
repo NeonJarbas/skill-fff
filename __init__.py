3a55bc20079dcb0147aec67faba01be0716d7b14
jarbasai <jarbasai@mailfence.com>
2023-01-06 12:34:16 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ class FullFreeFilmsSkill(OVOSCommonPlaybackSkill):
 
     def initialize(self):
         url = "https://www.youtube.com/channel/UCqBi1RfMbdXES6t2iMaIYSA"
-        bootstrap = f"https://raw.githubusercontent.com/OpenJarbas/streamindex/main/{self.archive.db.name}.json"
+        bootstrap = "https://github.com/JarbasSkills/skill-fff/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
         self.archive.monitor(url)
         self.archive.setDaemon(True)
